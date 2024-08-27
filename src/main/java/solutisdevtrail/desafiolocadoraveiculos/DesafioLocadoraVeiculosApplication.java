421732cf265afdcfeff01a05cf6d47255845b239
Pedro Renan <pedroviana15094@gmail.com>
2024-08-27 18:45:53 -0300

    init commit

---
+++ src/main/java/solutisdevtrail/desafiolocadoraveiculos/DesafioLocadoraVeiculosApplication.java
@@ -0,0 +1,13 @@
+package solutisdevtrail.desafiolocadoraveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioLocadoraVeiculosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesafioLocadoraVeiculosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/solutisdevtrail/desafiolocadoraveiculos/DesafioLocadoraVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package solutisdevtrail.desafiolocadoraveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioLocadoraVeiculosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
