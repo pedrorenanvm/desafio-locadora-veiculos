1cfa689aaf0668ed6484ce66421758d04b9d3dee
OscarDeBrito <oscarcorreiadebrito@gmail.com>
2024-08-30 21:59:19 -0300

    feat: Adiciona controller,Model,Service,DTO e Repository de motorista

--- src/main/java/solutisdevtrail/desafiolocadoraveiculos/repository/MotoristaRepository.java
+++ src/main/java/solutisdevtrail/desafiolocadoraveiculos/repository/MotoristaRepository.java
@@ -1,7 +1,7 @@
-//package solutisdevtrail.desafiolocadoraveiculos.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import solutisdevtrail.desafiolocadoraveiculos.model.entity.Motorista;
-//
-//public interface MotoristaRepository extends JpaRepository<Motorista, Long> {
-//}
+package solutisdevtrail.desafiolocadoraveiculos.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import solutisdevtrail.desafiolocadoraveiculos.model.entity.Motorista;
+
+public interface MotoristaRepository extends JpaRepository<Motorista, Long> {
+}
