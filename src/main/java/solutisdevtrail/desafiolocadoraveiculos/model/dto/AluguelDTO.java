fc3bc16fb202e46311f72dace1f5b5d07cc37ee8
OscarDeBrito <oscarcorreiadebrito@gmail.com>
2024-08-30 22:41:48 -0300

    feat: Adiciona controller,Model,Service,DTO e Repository de aluguel

--- src/main/java/solutisdevtrail/desafiolocadoraveiculos/model/dto/AluguelDTO.java
+++ src/main/java/solutisdevtrail/desafiolocadoraveiculos/model/dto/AluguelDTO.java
@@ -1,7 +1,18 @@
 package solutisdevtrail.desafiolocadoraveiculos.model.dto;
 
-import java.util.Date;
-import java.math.BigDecimal;
+import solutisdevtrail.desafiolocadoraveiculos.model.entity.Aluguel;
 
-public record AluguelDTO(Date dataPedido, Date dataEntrega, Date dataDevolucao, BigDecimal valorTotal, Long carroId, Long motoristaId) {
+import java.math.BigDecimal;
+import java.util.Date;
+
+public record AluguelDTO(Long id, Date dataPedido, Date dataEntrega, Date dataDevolucao, BigDecimal valorTotal,
+                         //Long carroId,
+                         Long motoristaId, Long apoliceId) {
+    public AluguelDTO(Aluguel aluguel) {
+        this(aluguel.getId(), aluguel.getDataPedido(), aluguel.getDataEntrega(),
+                aluguel.getDataDevolucao(), aluguel.getValorTotal(),
+                //aluguel.getCarro().getId()
+                 aluguel.getMotorista().getId(),
+                aluguel.getApolice() != null ? aluguel.getApolice().getId() : null);
+    }
 }
--- src/main/java/solutisdevtrail/desafiolocadoraveiculos/model/entity/Aluguel.java
+++ src/main/java/solutisdevtrail/desafiolocadoraveiculos/model/entity/Aluguel.java
@@ -1,32 +1,33 @@
-//package solutisdevtrail.desafiolocadoraveiculos.model.entity;
-//import jakarta.persistence.*;
-//import lombok.Data;
-//
-//import java.math.BigDecimal;
-//
-//import java.util.Date;
-//@Data
-//@Entity
-//public class Aluguel {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private Date dataPedido;
-//    private Date dataEntrega;
-//    private Date dataDevolucao;
-//    private BigDecimal valorTotal;
-//
+package solutisdevtrail.desafiolocadoraveiculos.model.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Data
+@Entity
+public class Aluguel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Date dataPedido;
+    private Date dataEntrega;
+    private Date dataDevolucao;
+    private BigDecimal valorTotal;
+
 //    @ManyToOne
 //    @JoinColumn(name = "carro_id")
 //    private Carro carro;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "motorista_id")
-//    private Motorista motorista;
-//
-//    @OneToOne
-//    @JoinColumn(name = "apolice_id")
-//    private ApoliceSeguro apolice;
-//
-//}
+
+    @ManyToOne
+    @JoinColumn(name = "motorista_id")
+    private Motorista motorista;
+
+    @OneToOne
+    @JoinColumn(name = "apolice_id")
+    private ApoliceSeguro apolice;
+}
